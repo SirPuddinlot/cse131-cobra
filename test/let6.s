1855e89da7f3f5c3af5b4b0df6e85db790fa45e1
AlainZhangStudent <41776644+SirPuddinlot@users.noreply.github.com>
2025-10-15 20:03:01 -0700

    added back runtime stuff

---
+++ test/let6.s
@@ -0,0 +1,20 @@
+section .text
+global our_code_starts_here
+our_code_starts_here:
+  mov rax, 10
+  mov [rsp - 8], rax
+  mov rax, 99
+  mov [rsp - 24], rax
+  mov rax, [rsp - 24]
+  mov [rsp - 32], rax
+  mov rax, 1
+  add rax, [rsp - 32]
+  mov [rsp - 16], rax
+  mov rax, [rsp - 8]
+  mov [rsp - 32], rax
+  mov rax, 5
+  add rax, [rsp - 32]
+  mov [rsp - 24], rax
+  mov rax, [rsp - 24]
+
+  ret
